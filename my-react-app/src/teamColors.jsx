4e36f09fa2eabf477ad74e2d1363951a85a32710
Ali Hussain <alihussain@MacBookAir.lan>
2025-11-08 17:02:28 -0800

    [UPDATE] react app

--- my-react-app/src/teamColors.jsx
+++ my-react-app/src/teamColors.jsx
@@ -1,18 +1,34 @@
 const teamColors = {
-  "LAL": { background: "#552583", color: "#FDB927" }, // Purple / Gold
-  "PHI": { background: "#006BB6", color: "#ED174C" }, // Blue / Red
-  "OKC": { background: "#007AC1", color: "#F05133" }, // Blue / Orange
-  "MIL": { background: "#00471B", color: "#EEE1C6" }, // Green / Cream
-  "CLE": { background: "#860038", color: "#041E42" }, // Wine / Navy
-  "PHO": { background: "#E56020", color: "#1D1160" }, // Orange / Purple
-  "UTA": { background: "#002B5C", color: "#F9A01B" }, // Navy / Gold
-  "NYK": { background: "#006BB6", color: "#F58426" }, // Blue / Orange
-  "BOS": { background: "#007A33", color: "#BA9653" }, // Green / Gold
-  "MIN": { background: "#0C2340", color: "#236192" }, // Navy / Light Blue
-  "SAS": { background: "#C4CED4", color: "#000000" }, // Silver / Black
-  "DET": { background: "#C8102E", color: "#006BB6" }, // Red / Blue
-  "DEN": { background: "#0E2240", color: "#FEC524" }, // Navy / Gold
-  "HOU": { background: "#CE1141", color: "#000000" }  // Red / Black
+  "ATL": { background: "#E03A3E", color: "#C1D32F" }, // Hawks: Red / Volt Green
+  "BOS": { background: "#007A33", color: "#BA9653" }, // Celtics: Green / Gold
+  "BKN": { background: "#000000", color: "#FFFFFF" }, // Nets: Black / White
+  "CHA": { background: "#1D1160", color: "#00788C" }, // Hornets: Purple / Teal
+  "CHI": { background: "#CE1141", color: "#000000" }, // Bulls: Red / Black
+  "CLE": { background: "#860038", color: "#041E42" }, // Cavaliers: Wine / Navy
+  "DAL": { background: "#00538C", color: "#002B5E" }, // Mavericks: Royal Blue / Navy
+  "DEN": { background: "#0E2240", color: "#FEC524" }, // Nuggets: Navy / Gold
+  "DET": { background: "#C8102E", color: "#006BB6" }, // Pistons: Red / Blue
+  "GSW": { background: "#1D428A", color: "#FFC72C" }, // Warriors: Blue / Gold
+  "HOU": { background: "#CE1141", color: "#000000" }, // Rockets: Red / Black
+  "IND": { background: "#002D62", color: "#FDBB30" }, // Pacers: Navy / Gold
+  "LAC": { background: "#C8102E", color: "#1D428A" }, // Clippers: Red / Blue
+  "LAL": { background: "#552583", color: "#FDB927" }, // Lakers: Purple / Gold
+  "MEM": { background: "#5D76A9", color: "#12173F" }, // Grizzlies: Blue / Navy
+  "MIA": { background: "#98002E", color: "#F9A01B" }, // Heat: Red / Yellow
+  "MIL": { background: "#00471B", color: "#EEE1C6" }, // Bucks: Green / Cream
+  "MIN": { background: "#0C2340", color: "#236192" }, // Timberwolves: Navy / Blue
+  "NOP": { background: "#0C2340", color: "#85714D" }, // Pelicans: Navy / Gold
+  "NYK": { background: "#006BB6", color: "#F58426" }, // Knicks: Blue / Orange
+  "OKC": { background: "#007AC1", color: "#F05133" }, // Thunder: Blue / Orange
+  "ORL": { background: "#0077C0", color: "#C4CED4" }, // Magic: Blue / Silver
+  "PHI": { background: "#006BB6", color: "#ED174C" }, // 76ers: Blue / Red
+  "PHO": { background: "#E56020", color: "#1D1160" }, // Suns: Orange / Purple
+  "POR": { background: "#E03A3E", color: "#000000" }, // Trail Blazers: Red / Black
+  "SAC": { background: "#5A2D81", color: "#63727A" }, // Kings: Purple / Gray
+  "SAS": { background: "#C4CED4", color: "#000000" }, // Spurs: Silver / Black
+  "TOR": { background: "#CE1141", color: "#000000" }, // Raptors: Red / Black
+  "UTA": { background: "#002B5C", color: "#F9A01B" }, // Jazz: Navy / Gold
+  "WAS": { background: "#002B5C", color: "#E31837" }  // Wizards: Navy / Red
 };
 
 export default teamColors;
