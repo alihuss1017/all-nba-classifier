8134804b066402e7c37b31973dd6cda173d5b7c6
alihuss1017 <alihuss1017@gmail.com>
2026-06-18 17:55:10 -0700

    [ADD] new UI demo

--- prefect_deploy.py
+++
@@ -1,8 +0,0 @@
-from datetime import timedelta
-from prefect_flow import pipeline  
-
-
-if __name__ == "__main__":
-    pipeline.serve(name = "Weekly NBA Update",
-                   tags = ["onboarding"],
-                   interval = 60 * 60 * 24 * 7)
